dbb18326312675737913a1945b6ce1d7764a50da
Rohama Salfi <rohxama@gmail.com>
2024-10-31 19:35:36 -0700

    making actual react element

--- Day1/src/main.jsx
+++ Day1/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Actual method in react for rendering elements
+
+const reactElement = React.createElement (
+  'a',
+  { href: 'https://google.com', target: '_blank'},
+  "Click me"
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    /* <App /> */
+    reactElement
+
 )
