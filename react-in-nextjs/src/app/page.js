70694f5c1fcfc698083e2dd0db58631903886092
Rohama Salfi <rohxama@gmail.com>
2024-11-18 23:26:08 +0500

    Making a function and explore props in react

--- react-in-nextjs/src/app/page.js
+++ react-in-nextjs/src/app/page.js
@@ -1,14 +1,25 @@
 'use client'
 
 import React, { useState } from 'react'
+import Header from '@/components/Header'
+import Link from 'next/link'
 
 const page = () => {
+
+    const [logo, setlogo] = useState('GoodCo.')
 
     const [marks, setmarks] = useState(80)
     return (
         <>
+            <Header logo={logo} />
+            <div className='flex flex-col'>
+                <a href="About">About</a>
+                <a href="/Project">Project</a>
+                <a href="/Services">Services</a>
+            </div>
+
             <h1>My marks is {marks}</h1>
-            <button onClick={() => {setmarks(90)}} className='bg-white p-1 mt-3 text-black rounded-md'>Update</button>
+            <button onClick={() => { setmarks(90) }} className='bg-black p-1 mt-3 text-white rounded-md'>Update</button>
         </>
 
     )
