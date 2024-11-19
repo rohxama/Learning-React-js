6e66aedb02f0ba2804e0c2b75181eba9dcb66c1d
Rohama Salfi <rohxama@gmail.com>
2024-11-19 23:55:03 +0500

    Remove git file

---
+++ react-in-nextjs/src/app/About/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/Header'
+const About = () => {
+  return (
+    <>
+     <Header logo ={logo}/>
+    <p>About page</p>
+    </>
+  )
+}
+
+export default About
