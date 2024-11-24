0f513bd5c7418143b601fbb9c92b9f00b8f88d71
Rohama Salfi <rohxama@gmail.com>
2024-11-24 23:57:38 +0500

    Solving routing errors

--- react-in-nextjs/src/app/page.js
+++ react-in-nextjs/src/app/page.js
@@ -6,12 +6,10 @@ import Link from 'next/link'
 
 const page = () => {
 
-    const [logo, setlogo] = useState('GoodCo.')
-
     const [marks, setmarks] = useState(80)
     return (
         <>
-            <Header logo={logo} />
+            <Header />
             <h1>My marks is {marks}</h1>
             <button onClick={() => { setmarks(90) }} className='bg-black p-1 mt-3 text-white rounded-md'>Update</button>
         </>
--- react-in-nextjs/src/components/Header.js
+++ react-in-nextjs/src/components/Header.js
@@ -3,11 +3,11 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 
-const Header = (props) => {
+const Header = () => {
   return (
     <>
       <div className='bg-slate-500 h-20 px-20 flex items-center justify-between'>
-        <p className='text-4xl'>{props.logo}</p>
+        <p className='text-4xl'>GoodCo.</p>
         <div className='flex items-center gap-6'>
           <Link href= "About" className='text-2xl'>About</Link>
           <Link href= "Contact" className='text-2xl'>Contact</Link>
